f35302c56faa42effc6479c98cbef394373f55e6
luisccf <luisccf@me.com>
2016-12-05 18:10:26 -0200

    Adds pokemon json format test

--- tests/apis/test_pokemon_api.py
+++ tests/apis/test_pokemon_api.py
@@ -3,18 +3,29 @@ from app.models.pokemon import Pokemon
 from app.models.type import Type
 from factories.pokemon_factory import PokemonFactory
 from scripts.load_types import LoadTypes
+import validictory
+import json
+
 
 class TestPokemonApi(BaseTestCase):
 
 
     def setUp(self):
-        PokemonFactory.create_batch(10)
+        PokemonFactory.create_batch(1)
 
 
     def tearDown(self):
         Pokemon.query.delete()
 
 
-    def test_api_should_return_10_pokemon(self):
+    def test_api_should_return_1_pokemon(self):
         response = self.client.get('/api/pokemon/')
-        self.assertEqual(len(response.json['data']), 10)
+        self.assertEqual(len(response.json['data']), 1)
+
+
+    def test_each_pokemon_in_json_have_all_fields(self):
+        response = self.client.get("/api/pokemon/")
+        for pokemon in response.json['data']:
+            fields = ['ndex', 'name', 'total', 'hp', 'attack', 'defense', 'sp_attack', 'sp_defense', 'speed', 'type1', 'type2']
+            self.assertTrue(set(fields).issubset(pokemon))
+            self.assertEqual(len(pokemon), 11)
