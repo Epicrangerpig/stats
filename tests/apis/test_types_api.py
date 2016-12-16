cc6cc4ca1e636ad545bd2314da72e198f35d60cb
luisccf <luisccf@me.com>
2016-12-16 13:57:44 -0200

    Creates test for number of types returned in types api

---
+++ tests/apis/test_types_api.py
@@ -0,0 +1,20 @@
+from test_base import BaseTestCase
+from app.models.type import Type
+from factories.type_factory import TypeFactory
+from scripts.load_types import LoadTypes
+
+
+class TestTypesApi(BaseTestCase):
+
+
+    def setUp(self):
+        TypeFactory.create_batch(10)
+
+
+    def tearDown(self):
+        Type.query.delete()
+
+
+    def test_api_should_return_10_types(self):
+        response = self.client.get('/api/types/')
+        self.assertEqual(len(response.json['data']), 10)
