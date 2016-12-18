944ffb9c75f2fb59411ce758b6df7c836110824b
luisccf <luisccf@me.com>
2016-12-18 21:38:38 -0200

    Adds trailing slash to ajax url

--- app/static/js/table.js
+++ app/static/js/table.js
@@ -1,34 +1,34 @@
 $(document).ready(function() {
     $('#pokemon').DataTable({
-        "sAjaxSource": "/api/pokemon",
-        "pageLength": "20",
+        'sAjaxSource': '/api/pokemon/',
+        'pageLength': '20',
         fixedHeader: {
             header: true,
             headerOffset: 50
         },
-        "aoColumns": [
-            { "mData": "ndex", 'sTitle': 'Dex' },
-            { "mData": "forme", 'sTitle': 'Name' },
-            { "mData": "total", 'sTitle': 'Total' },
-            { "mData": "hp", 'sTitle': 'HP' },
-            { "mData": "attack", 'sTitle': 'Attack' },
-            { "mData": "defense", 'sTitle': 'Defense' },
-            { "mData": "sp_attack", 'sTitle': 'Sp. Atk.' },
-            { "mData": "sp_defense", 'sTitle': 'Sp. Def.' },
-            { "mData": "speed", 'sTitle': 'Speed' },
-            { "mData": "type1", 'sTitle': 'Type 1' },
-            { "mData": "type2", 'sTitle': 'Type 2' }
+        'aoColumns': [
+            { 'mData': 'ndex', 'sTitle': 'Dex' },
+            { 'mData': 'forme', 'sTitle': 'Name' },
+            { 'mData': 'total', 'sTitle': 'Total' },
+            { 'mData': 'hp', 'sTitle': 'HP' },
+            { 'mData': 'attack', 'sTitle': 'Attack' },
+            { 'mData': 'defense', 'sTitle': 'Defense' },
+            { 'mData': 'sp_attack', 'sTitle': 'Sp. Atk.' },
+            { 'mData': 'sp_defense', 'sTitle': 'Sp. Def.' },
+            { 'mData': 'speed', 'sTitle': 'Speed' },
+            { 'mData': 'type1', 'sTitle': 'Type 1' },
+            { 'mData': 'type2', 'sTitle': 'Type 2' }
         ],
-        "columnDefs": [
+        'columnDefs': [
             {
-                "targets": [9, 10],
-                "render": function(data, type, row) {
+                'targets': [9, 10],
+                'render': function(data, type, row) {
                     return data.length ? '<span class="type ' + data + '">' + data + '</span>' : '';
                 }
             },
             {
-                "targets": [3, 4, 5, 6, 7, 8],
-                "render": function(data, type, row) {
+                'targets': [3, 4, 5, 6, 7, 8],
+                'render': function(data, type, row) {
                     var statsLabels = ['hp', 'attack', 'defense', 'sp_attack', 'sp_defense', 'speed'],
                         stats = [];
                     for (var i = 0; i < statsLabels.length; i++)
