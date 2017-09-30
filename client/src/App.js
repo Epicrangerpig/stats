4de4e9b19d1ddbf687a3d8a7ba333092ab1aa9cc
luisccf <luisccf@me.com>
2017-09-30 10:42:33 -0300

    Adds bootstrap 4, react-select

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import axios from 'axios'
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      pokemon: []
+      pokemon: [],
+      selectedOption: null,
     }
   }
 
   getPokemonList() {
     axios.get(`${process.env.REACT_APP_API_URL}/api/pokemon`)
     .then((res) => { 
-      this.setState({pokemon: res.data.data}); 
+      this.setState({pokemon: res.data.data})
     })
     .catch((err) => { 
-      console.log(err); 
+      console.log(err)
     })
   }
 
@@ -25,20 +28,31 @@ class App extends Component {
     this.getPokemonList()
   }
 
+  logChange(value) {
+    this.setState({selectedOption: value})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          {
-            this.state.pokemon.map((pokemon) => {
-              return <p>{ pokemon.name }</p>
-            })
-          }
-        </p>
+      <div className="container">
+        <div className="row justify-content-md-center">
+          <div className="col-md-6">
+            <h1 style={{ marginTop: 80 }}>Select pokémon</h1>
+            <Select
+              multi={ true }
+              value={ this.state.selectedOption }
+              options={
+                this.state.pokemon.map(pokemon => {
+                  return {
+                    value: pokemon.id,
+                    label: `${pokemon.forme}`
+                    }
+                })
+              }
+              onChange={this.logChange.bind(this)}
+            />
+          </div>
+        </div>
       </div>
     );
   }
