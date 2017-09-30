946ea553455606ba52b3e76326ebb3eb7296bd6e
luisccf <luisccf@me.com>
2017-09-30 13:30:28 -0300

    Adds linter

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,32 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import axios from 'axios'
+import React, { Component } from 'react';
+import axios from 'axios';
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
-import 'react-select/dist/react-select.css'
+import './App.css';
+
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       pokemon: [],
       selectedOption: null,
       loadingPokemon: true,
-    }
+    };
+  }
+
+  componentDidMount() {
+    this.getPokemonList();
   }
 
   getPokemonList() {
     axios.get(`${process.env.REACT_APP_API_URL}/api/pokemon`)
-    .then((res) => { 
-      this.setState({pokemon: res.data.data, loadingPokemon: false})
-    })
-    .catch((err) => { 
-      console.log(err)
-    })
-  }
-
-  componentDidMount() {
-    this.getPokemonList()
+      .then((res) => {
+        this.setState({ pokemon: res.data.data, loadingPokemon: false });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   render() {
@@ -37,23 +36,23 @@ class App extends Component {
           <div className="col-md-6">
             <h1 style={{ marginTop: 80 }}>Select pokémon</h1>
             <Select
-              multi={ true }
-              value={ this.state.selectedOption }
+              multi
+              value={this.state.selectedOption}
               isLoading={this.state.loadingPokemon}
               options={
-                this.state.pokemon.map(pokemon => {
-                  return {
+                this.state.pokemon.map(pokemon =>
+                  ({
                     value: pokemon.id,
-                    label: `${pokemon.forme}`
-                    }
-                })
+                    label: `${pokemon.forme}`,
+                  }),
+                )
               }
               onChange={value => this.setState({selectedOption: value})}
             />
           </div>
         </div>
         <div className="row justify-content-center">
-            <button className="btn btn-secondary" style={{ marginTop: 20 }}>compare</button>
+          <button className="btn btn-secondary" style={{ marginTop: 20 }}>compare</button>
         </div>
       </div>
     );
