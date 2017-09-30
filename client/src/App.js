9f43171c234c4580cd7ce13d2c3e12c116bf9850
luisccf <luisccf@me.com>
2017-09-30 13:59:47 -0300

    Adds About component and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Select from 'react-select';
+import { Route, Switch } from 'react-router-dom'
 import 'react-select/dist/react-select.css';
 import './App.css';
-
+import About from './components/About';
 
 class App extends Component {
   constructor(props) {
@@ -31,29 +32,39 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-md-6">
-            <h1 style={{ marginTop: 80 }}>Select pokémon</h1>
-            <Select
-              multi
-              value={this.state.selectedOption}
-              isLoading={this.state.loadingPokemon}
-              options={
-                this.state.pokemon.map(pokemon =>
-                  ({
-                    value: pokemon.id,
-                    label: `${pokemon.forme}`,
-                  }),
-                )
-              }
-              onChange={value => this.setState({selectedOption: value})}
-            />
-          </div>
-        </div>
-        <div className="row justify-content-center">
-          <button className="btn btn-secondary" style={{ marginTop: 20 }}>compare</button>
-        </div>
+      <div className="container" style={{ marginTop: 80 }}>
+        <Switch>
+          <Route 
+            exact 
+            path="/" 
+            render={() => (
+              <div className="row justify-content-center">
+                <div className="col-md-6">
+                  <h1>Select pokémon</h1>
+                  <Select
+                    multi
+                    value={this.state.selectedOption}
+                    isLoading={this.state.loadingPokemon}
+                    options={
+                      this.state.pokemon.map(pokemon =>
+                        ({
+                          value: pokemon.id,
+                          label: `${pokemon.forme}`,
+                        }),
+                      )
+                    }
+                    onChange={value => this.setState({ selectedOption: value })}
+                  />                  
+                  <div className="row justify-content-center">
+                    <button className="btn btn-secondary" style={{ marginTop: 20 }}>compare</button>
+                  </div>
+                </div>
+              </div>
+            )}
+          />
+
+          <Route exact path="/about" component={About} />
+        </Switch>
       </div>
     );
   }
